e2f0c20c27afed41b2fb9a8974996bc102efbd61
Rachel Fernandez <rachel.a.fernandez@gmail.com>
2024-02-20 16:39:52 -0800

    hi

--- Crescendo/src/main/java/frc/robot/subsystems/Drive.java
+++ Crescendo/src/main/java/frc/robot/subsystems/Drive.java
@@ -87,12 +87,6 @@ public class Drive extends SubsystemBase {
   public ChassisSpeeds getRelativeSpeeds(){
     return DRIVE_KINEMATICS.toChassisSpeeds(getModuleStates());
   }
-
-  SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(DRIVE_KINEMATICS, this.getDriveHeading(), this.getModulePositions());
-
-  public Pose2d getPose2D(){
-    return m_odometry.getPoseMeters();
-  }
   
   public void readConfigGains(){
     m_topLeft.configGains();
@@ -100,10 +94,6 @@ public class Drive extends SubsystemBase {
     m_bottomLeft.configGains();
     m_bottomRight.configGains();
 
-  }
-
-  public void resetOdometry(Pose2d pose){
-    m_odometry.resetPosition(getDriveHeading(), getModulePositions(), pose);
   }
   
   public void driveFromChassis(ChassisSpeeds speeds){
@@ -131,33 +121,7 @@ public class Drive extends SubsystemBase {
     SmartDashboard.putNumber("rear right", m_bottomRight.getTurnAngle().getDegrees());
   }
 
-  public void autonomousRoutine() {
-    AutoBuilder.configureHolonomic(
-                this::getPose2D, // Robot pose supplier
-                this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-                this::getRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-                this::driveFromChassis, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-                        4.5, // Max module speed, in m/s
-                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-                        new ReplanningConfig() // Default path replanning config. See the API for the options here
-                ),
-                () -> {
-                    // Boolean supplier that controls when the path will be mirrored for the red alliance
-                    // This will flip the path being followed to the red side of the field.
-                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-                    var alliance = DriverStation.getAlliance();
-                    if (alliance.isPresent()) {
-                        return alliance.get() == DriverStation.Alliance.Red;
-                    }
-                    return false;
-                },
-                this // Reference to this subsystem to set requirements
-        );
-  }
+  
 
   @Override
   public void periodic() {
---
+++ Crescendo/src/main/java/frc/robot/subsystems/PathHandler.java
@@ -0,0 +1,69 @@
+package frc.robot.subsystems;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+
+public class PathHandler {
+    private static PathHandler instance = new PathHandler();
+
+    private Tracker tracker = Tracker.getInstance();
+    private Drive drive = Drive.getInstance();
+
+    public PathHandler(){
+        configEvents();
+        configAutoBuilder();
+    }
+
+    public static PathHandler getInstance(){
+        return instance;
+    }
+
+    public Command getAuto(String fileName){
+        return AutoBuilder.buildAuto(fileName);
+    }
+
+    public Command followPath(String pathName){
+        PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
+        return AutoBuilder.followPath(path);
+    }
+
+    public void configEvents(){
+        NamedCommands.registerCommand("print", new PrintCommand("Hello World!"));
+    }
+
+    public void configAutoBuilder(){
+        AutoBuilder.configureHolonomic(
+                tracker::getPose, // Robot pose supplier
+                tracker::setPose, // Method to reset odometry (will be called if your auto has a starting pose)
+                drive::getRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                drive::driveFromChassis, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                drive // Reference to this subsystem to set requirements
+        );
+    }
+}
