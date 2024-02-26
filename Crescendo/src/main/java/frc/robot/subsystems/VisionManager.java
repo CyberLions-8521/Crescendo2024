913d88101a2225a45852af38bf0bd12401e8671b
jackhnguyen <119372912+jackhnguyen@users.noreply.github.com>
2024-02-26 11:18:11 -0800

    more vision

--- Crescendo/src/main/java/frc/robot/subsystems/VisionManager.java
+++ Crescendo/src/main/java/frc/robot/subsystems/VisionManager.java
@@ -4,17 +4,31 @@
 
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonUtils;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveModuleConstants;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagPoseEstimate;
 
 
 public class VisionManager extends SubsystemBase {
@@ -25,38 +39,54 @@ public class VisionManager extends SubsystemBase {
   private PhotonCamera m_camera2 = new PhotonCamera("Jackson");
   private PhotonPipelineResult results1;
   private PhotonPipelineResult results2;
-  private boolean hasTargets1;
-  private boolean hasTargets2;
-  Pose3d robotPose;
-  PhotonTrackedTarget target1;
-  PhotonTrackedTarget target2;
+  private Pose3d robotPose;
+  private PhotonTrackedTarget target1;
+  private PhotonTrackedTarget target2;
+  Transform3d robotToCam1;
+  Transform3d robotToCam2;
 
-  public VisionManager() {
+  PhotonPoseEstimator m_estimator1 = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, m_camera1, robotToCam1);
+  PhotonPoseEstimator m_estimator2 = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, m_camera2, robotToCam2);
+  SwerveDrivePoseEstimator m_swerveEstimator;
+  
+  public VisionManager(Rotation2d gyroAngle, SwerveModulePosition[] swervePositions) {
 
+    m_swerveEstimator = new SwerveDrivePoseEstimator(SwerveModuleConstants.DRIVE_KINEMATICS, gyroAngle, swervePositions, null);
   }
 
   @Override
   public void periodic() {
+    update(null, null);
+  }
+
+  public void update(Rotation2d gyroAngle, SwerveModulePosition[] modulePositions){
+    m_swerveEstimator.update(gyro.getRotation2d(), modulePositions);
+    
     results1 = m_camera1.getLatestResult();
     results2 = m_camera2.getLatestResult();
-    hasTargets1 = results1.hasTargets();
-    hasTargets2 = results2.hasTargets();
     
-    if(hasTargets1){
+    //add smth for that speific aiming tag on the speaker
+    if(results1.hasTargets()){
       target1 = results1.getBestTarget();
       robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target1.getBestCameraToTarget(), 
       aprilTagFieldLayout.getTagPose(target1.getFiducialId()).get(), new Transform3d());
     }
 
-    if(hasTargets2){
+    if(results2.hasTargets()){
       target2 = results2.getBestTarget();
       robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target1.getBestCameraToTarget(), 
       aprilTagFieldLayout.getTagPose(target1.getFiducialId()).get(), new Transform3d());
     }
-    
+    m_swerveEstimator.addVisionMeasurement(null, 0);
   }
 
-  @Override
-  public void simulationPeriodic() {
-  }
+
+
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    AprilTagPoseEstimate estimate = new AprilTagPoseEstimate(null, null, 0, 0)   
+    m_estimator1.setReferencePose(prevEstimatedRobotPose);
+        return m_estimator1.update();
+    }
+
+
 }
