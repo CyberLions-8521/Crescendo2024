76b742a0ecc6a6e8ce3fa40e5cbe2951d13b988d
ktvuwhs <ktvu@hbuhsd.edu>
2024-05-07 09:07:53 -0700

    commented unused import

--- src/main/java/frc/robot/subsystems/Toaster.java
+++ src/main/java/frc/robot/subsystems/Toaster.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import frc.robot.Constants.MotorConstants;
 import frc.robot.Constants.ToasterConstants;
